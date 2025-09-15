9fefe97a77c653d1c456a9fad73c092012335268
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2025-09-15 12:06:27 -0300

    prueba swa

--- vite.config.js
+++ vite.config.js
@@ -8,24 +8,6 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate', // fuerza actualización automática de la app
       includeAssets: ['favicon.ico', 'robots.txt'], // archivos extra a cachear
-      manifest: {
-        name: 'Mi App React',
-        short_name: 'AppReact',
-        description: 'Mi app con PWA',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
       workbox: {
         clientsClaim: true,
         skipWaiting: true,
