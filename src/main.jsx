ebeab7f0077471c108f5e6580f8b6b47c8fc1c56
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2025-09-15 18:36:02 -0300

    prueba swa

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let reg of registrations) {
+      if (reg.scope.includes('/ingreso/')) {
+        console.log("Desregistrando service worker de /ingreso");
+        reg.unregister();
+      }
+    }
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />,
