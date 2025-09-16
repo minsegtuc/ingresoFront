29f1f9ddc89160286a337a2b342afc99a86654d3
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2025-09-15 21:05:35 -0300

    prueba swa

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate', // fuerza actualización automática de la app
+      scope: '/ingreso/',
+      start_url: '/ingreso/',
       includeAssets: ['favicon.ico', 'robots.txt'], // archivos extra a cachear
       workbox: {
         clientsClaim: true,
